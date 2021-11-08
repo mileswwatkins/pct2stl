7d63e560e0abf60622b203ead3938464529915a6
Miles Watkins <miles.w.watkins@gmail.com>
2021-11-07 16:56:03 -0800

    Add cleanup steps

--- pct2stl.sh
+++ pct2stl.sh
@@ -39,7 +39,7 @@ ogr2ogr \
             SELECT ST_UNARYUNION(ST_COLLECT(geometry)) AS geometry
             FROM PacificCrestTrail
         )
-        SELECT ST_BUFFER(geometry, 20000)
+        SELECT ST_BUFFER(geometry, 25000)
         FROM \"pct-unioned\"
     "
 
@@ -48,38 +48,53 @@ ogr2ogr \
 # the "right" way to do this is with the `BuildCircleMbr` SpatiaLite
 # function, but that is currently resulting in rectangles instead.
 ogr2ogr \
-    data/pct-terminus-holes.shp \
+    data/pct-terminus-holes-mask.shp \
     data/PacificCrestTrail.shp \
     -a_srs EPSG:4326 \
     -dialect sqlite \
     -sql "
-        SELECT ST_UNION(
-            BUFFER(ST_POINT(-120.799, 49.000), 0.1),
-            BUFFER(ST_POINT(-116.467, 32.589), 0.1)
-        )
-    "
-# TO DO: The holes aren't getting punched here; come back and fix this
-ogr2ogr \
-    data/pct-buffered-clipped.shp \
-    data/pct-buffered.shp \
-    -dialect sqlite \
-    -sql "
-        SELECT ST_SYMDIFFERENCE(
-            A.geometry,
-            B.geometry
-        )
-        FROM \"pct-buffered\" AS A,
-            \"data/pct-terminus-holes.shp\".pct-terminus-holes AS B
+        SELECT ST_DIFFERENCE(
+            ST_POLYGONFROMTEXT(
+                'POLYGON((
+                    -125.0011 24.9493,
+                    -125.0011 49.5904,
+                    -66.9326 49.5904,
+                    -66.9326 24.9493,
+                    -125.0011 24.9493
+                ))'
+            ),
+            ST_UNION(
+                BUFFER(ST_POINT(-120.799, 49.000), 0.1),
+                BUFFER(ST_POINT(-116.467, 32.589), 0.1)
+            )
+        ) AS geometry
     "
 
 if [ -f data/pct-dem-trimmed.tif ]; then
     rm data/pct-dem-trimmed.tif
 fi
 gdalwarp \
-    -cutline data/pct-buffered-clipped.shp \
-    -cl pct-buffered-clipped \
+    -cutline data/pct-buffered.shp \
+    -cl pct-buffered \
     -crop_to_cutline \
     data/pct-dem.tif \
     data/pct-dem-trimmed.tif
+gdalwarp \
+    -cutline data/pct-terminus-holes-mask.shp \
+    -cl pct-terminus-holes-mask \
+    -crop_to_cutline \
+    data/pct-dem-trimmed.tif \
+    data/pct-dem-trimmed-holed.tif
+gdal_edit.py \
+    -a_nodata 0 \
+    data/pct-dem-trimmed-holed.tif
+# TO DO: Trim this very large bounding box down to just the area
+# _without_ nodata
+# TO DO: Find a way to slice off the bottom of the QGIS-generated
+# STL polygon
 
+# Suggested settings for DEMto3D:
+# https://edutechwiki.unige.ch/en/3D_printing_of_digital_elevation_models_with_QGIS#Procedure_for_using_DEMto3D
+# Note that Shapeways has a maximum size for metal printing of
+# 89 × 89 × 100 mm; see, eg, https://www.shapeways.com/materials/bronze
 echo "Now manually use the DEMto3D QGIS plugin to create an STL file"
